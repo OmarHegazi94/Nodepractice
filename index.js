aba7be462719970ec754934549c06ec6e1bf4ef6
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2020-01-06 18:19:01 +0200

    simple node api

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+///////////////// FILES
 
 // Blocking , synchronous way
 
@@ -12,20 +16,69 @@ const fs = require("fs");
 
 // Non-blocking , asynchronous way
 
-fs.readFile('./txt/start.txt', 'utf-8' ,(error, data1) => {
-    if(error) return console.log("ERROR");
+// fs.readFile('./txt/start.txt', 'utf-8' ,(error, data1) => {
+//     if(error) return console.log("ERROR");
 
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
-        console.log(data2);
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+//         console.log(data2);
 
-        fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
-            console.log(data3);
+//         fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
+//             console.log(data3);
 
-            fs.writeFile('./txt/final.txt', `${data2} \n${data3}`, 'utf-8', error => {
-                console.log("your data has been written 🥑");
-            });
+//             fs.writeFile('./txt/final.txt', `${data2} \n${data3}`, 'utf-8', error => {
+//                 console.log("your data has been written 🥑");
+//             });
+//         });
+//     });
+// });
+
+// console.log("will read first");
+
+
+///////////////// HTTP
+// basic routing
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
+const server = http.createServer((request, response) => {
+    const pathName = request.url;
+
+
+    if(pathName === "/" || pathName === "/overview"){
+        response.end("This is the overview");
+
+    } else if(pathName === "/product"){
+        response.end("This is the product");
+    } 
+     else if(pathName === "/api"){
+
+        response.writeHead(200, {
+            'Content-type': 'application/json'
         });
-    });
+        response.end(data);
+
+        // fs.readFile("./dev-data/data.json"); bad way
+        // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (error, data) => {
+        //     const productData = JSON.parse(data);
+        //     // console.log(productData);
+        //     response.writeHead(200, {
+        //         'Content-type': 'application/json'
+        //     });
+        //     response.end(data);
+
+        // });
+    } else {
+        // response.writeHead(404); send 404 status error
+        response.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello header'
+        });
+        // response.end("page not found!");
+        response.end("<h1>page not found!</h1>");
+    }
 });
 
-console.log("will read first");
+server.listen(8000, "127.0.0.1", ()=> {
+    console.log("listening for request on port 8000");
+});
